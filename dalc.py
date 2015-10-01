8364aa2e8ab71671ada7272c623b4222e33a0ce6
Pascal Germain <pascal.germain@gmail.com>
2015-10-01 14:58:57 -0400

    DALC: A new PAC-Bayesain domain adaptation algorithm

---
+++ dalc.py
@@ -0,0 +1,215 @@
+#-*- coding:utf-8 -*-
+'''
+DOMAIN ADAPTATION LEARNING OF LINEAR CLASSIFIERS (aka DALC)
+See: http://arxiv.org/abs/1506.04573
+
+Learning algorithm implementation
+
+@author: Pascal Germain -- http://graal.ift.ulaval.ca/pgermain
+'''
+from kernel import Kernel, KernelClassifier
+import numpy as np
+from math import sqrt, pi
+from numpy import exp, maximum
+from scipy.special import erf
+from scipy import optimize
+from collections import OrderedDict
+
+# Some useful constants
+CTE_1_SQRT_2    = 1.0 / sqrt(2.0)
+CTE_1_SQRT_2PI  = 1.0 / sqrt(2 * pi)
+CTE_SQRT_2_PI   = sqrt(2.0 / pi)
+
+# Some useful functions, and their derivatives
+def gaussian_loss(x):
+    return 0.5 * ( 1.0 - erf(x * CTE_1_SQRT_2) )
+
+def gaussian_loss_derivative(x):
+    return -CTE_1_SQRT_2PI * exp(-0.5 * x**2)
+
+def gaussian_convex_loss(x):
+    return maximum( 0.5*(1.0-erf(x*CTE_1_SQRT_2)) , -x*CTE_1_SQRT_2PI+0.5 )
+
+def gaussian_convex_loss_derivative(x):
+    x = maximum(x, 0.0)
+    return -CTE_1_SQRT_2PI * exp(-0.5 * x**2)
+
+def gaussian_disagreement(x):
+    return 0.5 * ( 1.0 - (erf(x * CTE_1_SQRT_2))**2 )
+
+def gaussian_disagreement_derivative(x):
+    return -CTE_SQRT_2_PI * erf(x * CTE_1_SQRT_2) * exp(-0.5 * x**2)
+
+def gaussian_joint_error(x):
+    return 0.25 * ( 1.0 - erf(x * CTE_1_SQRT_2) )**2
+
+def gaussian_joint_error_derivative(x):
+    return -CTE_1_SQRT_2PI * exp(-0.5 * x**2)  * ( 1.0 - erf(x * CTE_1_SQRT_2) )
+
+JE_SADDLE_POINT_X = -0.5060544689891808
+JE_SADDLE_POINT_Y = gaussian_joint_error(JE_SADDLE_POINT_X)
+JE_SADDLE_POINT_DX = gaussian_joint_error_derivative(JE_SADDLE_POINT_X)
+
+def gaussian_joint_error_convex(x):
+    return maximum( gaussian_joint_error(x), JE_SADDLE_POINT_DX * (x - JE_SADDLE_POINT_X) + JE_SADDLE_POINT_Y)
+
+def gaussian_joint_error_convex_derivative(x):
+    return gaussian_joint_error_derivative( maximum(x, JE_SADDLE_POINT_X) )
+
+
+# Main learning algorithm
+class Dalc:
+    def __init__(self, B=1.0, C=1.0, convexify=False, nb_restarts=1, verbose=False):
+        """Pbda learning algorithm.
+        B: Trade-off parameter 'B' (source joint error modifier)
+        C: Trade-off parameter 'C' (target disagreement modifier)
+        convexify: If True, the source loss function is convexified (False by default)
+        nb_restarts: Number of random restarts of the optimization process.
+        verbose: If True, output informations. Otherwise, stay quiet.
+        """       
+        self.B  = float(B)
+        self.C  = float(C)
+        
+        self.nb_restarts = int(nb_restarts)
+        self.verbose     = bool(verbose)
+
+        if convexify:
+            self.source_loss_fct = gaussian_joint_error_convex
+            self.source_loss_derivative_fct = gaussian_joint_error_convex_derivative
+        else:
+            self.source_loss_fct = gaussian_joint_error
+            self.source_loss_derivative_fct = gaussian_joint_error_derivative
+
+        
+    def learn(self, source_data, target_data, kernel=None, return_kernel_matrix=False):
+        """Launch learning process."""
+        if kernel is None: kernel = 'linear'
+
+        if type(kernel) is str:
+            kernel = Kernel(kernel_str=kernel)
+
+        if self.verbose: print('Building kernel matrix.')
+        data_matrix  = np.vstack(( source_data.X, target_data.X) )
+        label_vector = np.hstack(( source_data.Y, np.zeros(target_data.get_nb_examples()) ))
+        
+        kernel_matrix = kernel.create_matrix(data_matrix)
+        
+        alpha_vector = self.learn_on_kernel_matrix(kernel_matrix, label_vector)
+
+        classifier =  KernelClassifier(kernel, data_matrix, alpha_vector)
+        if return_kernel_matrix:
+            return classifier, kernel_matrix
+        else:
+            return classifier
+
+    def learn_on_kernel_matrix(self, kernel_matrix, label_vector):  
+        """Launch learning process, from a kernel matrix. In label_vector, 0 indicates target examples."""                     
+        self.kernel_matrix = kernel_matrix
+        self.label_vector = np.array(label_vector, dtype=int)
+        self.target_mask = np.array( self.label_vector == 0, dtype=int )
+        self.source_mask = np.array( self.label_vector != 0, dtype=int )
+
+        self.nb_examples = len( self.label_vector )
+                
+        if np.shape(kernel_matrix) != (self.nb_examples, self.nb_examples):
+            raise Exception("kernel_matrix and label_vector size differ.")
+        
+        self.margin_factor = (self.label_vector + self.target_mask) / np.sqrt( np.diag(self.kernel_matrix) )
+        
+        initial_vector = self.label_vector / float(self.nb_examples)        
+        best_cost, best_output = self.perform_one_optimization(initial_vector, 0)
+        
+        for i in range(1, self.nb_restarts):
+            initial_vector = (np.random.rand( self.nb_examples ) -.5) / self.nb_examples
+            cost, optimizer_output = self.perform_one_optimization(initial_vector, i)
+            
+            if cost < best_cost:
+                best_cost = cost
+                best_output = optimizer_output        
+        
+        self.optimizer_output = best_output
+        self.alpha_vector = best_output[0]       
+        return self.alpha_vector
+    
+    def perform_one_optimization(self, initial_vector, i):
+        """Perform a optimization round."""  
+        if self.verbose: print('Performing optimization #' + str(i+1) + '.')
+        
+        optimizer_output = optimize.fmin_l_bfgs_b(self.calc_cost, initial_vector, self.calc_gradient) 
+        cost = optimizer_output[1] 
+        
+        if self.verbose:
+            print('cost value: ' +  str(cost))
+            for (key, val) in optimizer_output[2].items():
+                if key is not 'grad': print(str(key) + ': ' + str(val))                    
+    
+        return cost, optimizer_output
+                           
+    def calc_cost(self, alpha_vector, full_output=False):
+        """Compute the cost function value at alpha_vector."""
+        kernel_matrix_dot_alpha_vector = np.dot( self.kernel_matrix, alpha_vector )
+        margin_vector = kernel_matrix_dot_alpha_vector * self.margin_factor
+        
+        joint_err_vector = self.source_loss_fct(margin_vector) * self.source_mask
+        loss_source = joint_err_vector.sum()
+
+        disagreement_vector = gaussian_disagreement(margin_vector) * self.target_mask
+        loss_target = disagreement_vector.sum()
+
+        KL = np.dot(kernel_matrix_dot_alpha_vector, alpha_vector) / 2
+               
+        cost = loss_source / self.C + loss_target / self.B + KL / (self.B * self.C)
+
+        if full_output:
+            return cost, loss_source, loss_target, KL
+        else:
+            return cost
+        
+    def calc_gradient(self, alpha_vector):
+        """Compute the cost function gradient at alpha_vector."""
+        kernel_matrix_dot_alpha_vector = np.dot( self.kernel_matrix, alpha_vector )
+        margin_vector = kernel_matrix_dot_alpha_vector * self.margin_factor
+
+        d_joint_err_vector = self.source_loss_derivative_fct( margin_vector ) * self.margin_factor * self.source_mask
+        d_loss_source_vector = np.dot(d_joint_err_vector, self.kernel_matrix)
+                        
+        d_dis_vector = gaussian_disagreement_derivative( margin_vector ) * self.margin_factor * self.target_mask
+        d_loss_target_vector = np.dot(d_dis_vector, self.kernel_matrix)
+                          
+        d_KL_vector = kernel_matrix_dot_alpha_vector
+
+        return d_loss_source_vector / self.C + d_loss_target_vector / self.B + d_KL_vector / (self.B * self.C)
+        
+    def get_stats(self, alpha_vector=None):
+        """Compute some statistics."""
+        if alpha_vector is None: alpha_vector = self.alpha_vector
+        cost, loss_source, loss_target, KL = self.calc_cost(alpha_vector, full_output=True)
+        nb_examples_source = int(np.sum(self.source_mask))
+
+        stats = OrderedDict()
+
+        stats['B'] = self.B
+        stats['C'] = self.C
+        stats['cost value'] = cost
+        stats['loss source'] = loss_source / nb_examples_source
+        stats['loss target'] = loss_target / self.nb_examples
+        stats['source loss fct'] = self.source_loss_fct.__name__
+        stats['KL'] = KL
+        stats['optimizer warnflag'] = self.optimizer_output[2]['warnflag']
+
+        return stats
+        
+                  
+if __name__ == '__main__':
+    from dataset import Dataset
+    from kernel import Kernel
+
+    from moons_utils import draw_moons_grid
+
+    def my_learn_function(X, y, Xt, _B):
+        algo = Dalc(verbose=True, B=_B, C=1., convexify=False, joint_loss=True)
+        clf = algo.learn(Dataset(X,y), Dataset(Xt), kernel=Kernel('rbf', gamma=1.))
+        print(algo.get_stats())
+        return clf.predict
+
+    draw_moons_grid(my_learn_function, [1.,], [10,30,50])
---
+++ dalc_learn.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+'''
+DOMAIN ADAPTATION LEARNING OF LINEAR CLASSIFIERS (aka DALC)
+See: http://arxiv.org/abs/1506.04573
+
+Executable script to launch the learning algorithm
+
+@author: Pascal Germain -- http://graal.ift.ulaval.ca/pgermain
+'''
+import common
+from dalc import *
+from dataset import *
+from kernel import *
+
+import sys
+import pickle
+import argparse 
+
+# common.print_header('LEARNING ALGORITHM')
+
+# Arguments parser
+parser = argparse.ArgumentParser(description="", formatter_class=common.custom_formatter, epilog="")
+
+parser.add_argument("-b", dest="B_value", type=float, default=1.0, help="Trade-off parameter \"B\" (source joint error modifier). Default: 1.0")
+
+parser.add_argument("-c", dest="C_value", type=float, default=1.0, help="Trade-off parameter \"C\" (target disagreement modifier). Default: 1.0")
+
+parser.add_argument("--kernel", "-k", dest="kernel", default="linear", choices=['rbf', 'linear'], help="Kernel function. Default: linear.")
+parser.add_argument("--gamma",  "-g", dest="gamma",  type=float, default=1.0, help="Gamma parameter of the RBF kernel. Only used if --kernel is set to rbf. Default: 1.0")
+parser.add_argument("--nb_restarts", "-n", dest="nb_restarts",  type=int, default=1, help='Number of random restarts of the optimization process. Default: 1')
+parser.add_argument("--format", "-f", dest="format",  choices=['matrix', 'svmlight'], default='matrix', help='Datasets format. Default: matrix (each line defines an example, the first column defines the label in {-1, 1}, and the next columns represent the real-valued features)')
+parser.add_argument("--model",  "-m", dest="model_file", default='model.bin', help="Model file name. Default: model.bin")
+parser.add_argument("--weight", "-w", dest="weight_file", default='', help="Weight vector file name. Default: (none)")
+
+parser.add_argument("source_file", help="Defines the file containing the source dataset.")
+parser.add_argument("target_file", help="Defines the file containing the target dataset.")
+args = parser.parse_args()
+
+# Main program
+###############################################################################
+print('... Loading dataset files ...')
+###############################################################################
+try:
+    if args.format == 'matrix':
+        source_data = dataset_from_matrix_file(args.source_file)
+    elif args.format == 'svmlight':   
+        source_data = dataset_from_svmlight_file(args.source_file)
+except:
+    print('ERROR: Unable to load source file "' + args.source_file + '".')
+    sys.exit(-1)
+ 
+print(str(source_data.get_nb_examples()) + ' source examples loaded.')
+
+try:
+    if args.format == 'matrix':
+        target_data = dataset_from_matrix_file(args.target_file)
+    elif args.format == 'svmlight':
+        target_data = dataset_from_svmlight_file(args.target_file, source_data.get_nb_features())
+        source_data.reshape_features(target_data.get_nb_features())
+except:
+    print('ERROR: Unable to load target file "' + args.target_file + '".')
+    sys.exit(-1)
+ 
+print(str(target_data.get_nb_examples()) + ' target examples loaded.')  
+
+###############################################################################
+print('\n... Learning ...')
+###############################################################################
+if args.kernel == 'rbf':
+    kernel = Kernel('rbf', gamma=args.gamma)
+elif args.kernel == 'linear':
+    kernel = Kernel('linear')
+    
+algo = Dalc(C=args.C_value, B=args.B_value, verbose=True, nb_restarts=args.nb_restarts )
+classifier = algo.learn(source_data, target_data, kernel) 
+
+###############################################################################
+print('\n... Saving model: "' + args.model_file + '" ...')
+###############################################################################
+try:
+    with open(args.model_file, 'wb') as model:
+        pickle.dump(classifier, model, pickle.HIGHEST_PROTOCOL)
+    print('File "' + args.model_file + '" created.')
+except:
+    print('ERROR: Unable to write model file "' + args.model_file + '".')
+
+if len(args.weight_file) > 0:
+    try:
+        classifier.write_to_file(args.weight_file)
+        print('File "' + args.weight_file + '" created.')
+    except:
+        print('ERROR: Unable to write weight file "' + args.weight_file + '".')
+
+############################################################################### 
+print('\n... Computing statistics ...')
+###############################################################################
+stats_dict = algo.get_stats()
+
+for key,val in stats_dict.iteritems():
+    print( str(key) + ' = ' + str(val) )
+
+
